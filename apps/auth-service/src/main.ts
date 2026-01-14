b1c1641f84987995d0c87398778c15f902faf4c6
NabeelAttar <nabeelattar2411@gmail.com>
2026-01-14 18:19:36 +0530

    integrate Swagger for API documentation and add user registration route

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
 import cookieParser from 'cookie-parser';
+import router from './routes/auth.router';
+import swaggerUi from 'swagger-ui-express';
+const swaggerDocuemnt = require('./swagger-output.json');
 
 const app = express();
 
@@ -20,12 +23,23 @@ app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocuemnt));
+app.get('/docs-json', (req, res) => {
+  res.json(swaggerDocuemnt);
+})
+
+// add routes
+app.use('/api', router);
+// one api is ready now will write api docs 
+
 app.use(errorMiddleware);
 
 const port = process.env.PORT || 6001;
 const server = app.listen(port, () => {
     console.log(`auth service is running at http://localhost:${port}/api`);
+    console.log(`Swagger docs available at http://localhost:${port}/docs`);
 });
 server.on("error", (err) => {
     console.log("Server Error:", err);
+
 });
