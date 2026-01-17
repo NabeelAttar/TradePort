8b9dd1d26a2bb3066635b1200807640404dd7229
NabeelAttar <nabeelattar2411@gmail.com>
2026-01-17 15:50:14 +0530

    chore: implementing new service - user ui,  header component of the main app

---
+++ apps/user-ui/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import Header from '../shared/widgets';
+import './global.css';
+import { Poppins, Roboto } from 'next/font/google'
+
+export const metadata = {
+  title: 'TradePort',
+  description: 'TradePort',
+}
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+})
+
+const popppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} ${popppins.variable}`}>
+        <Header/>
+        {children}
+      </body>
+    </html>
+  )
+}
